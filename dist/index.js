9eb75599795b7ae34b5bd21b52dc0f7186d46d88
yuyu <yuyutw123@gmail.com>
2022-07-31 15:51:39 +0800

    message

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// route setting 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// setting static files
+app.use(express.static(__dirname + '/public'));
+
+//setting template engine
+app.engine('handlebars', exphbs.engine('defaultLayout: index'));
+app.set('view engine', 'handlebars');
+
+//Connect to database
+connectDB();
+
+app.use(express.json({ extented: false }));
+
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
